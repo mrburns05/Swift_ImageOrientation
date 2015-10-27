ca3d1b36d53189943f3b69638d7c35c132b43c1b
mrburns05 <mrburns05@gmail.com>
2015-10-26 19:53:01 -0700

    Swift 2.0 Extension To Fix ImageOrientation
    Original code was written in objC, i don’t remember who wrote it… So
    this is my swift interpretation..

---
+++ UIImage+FixOrientation.swift
@@ -0,0 +1,68 @@
+//
+//  UIImage+FixOrientation.swift
+//  PXPanel
+//
+//  Created by Jacob Burns on 10/26/15.
+//  Copyright © 2015 jeba. All rights reserved.
+//
+
+import UIKit
+
+extension UIImage {
+    
+    func fixOrientation() ->UIImage {
+        
+        if self.imageOrientation == UIImageOrientation.Up {
+            return self
+        }
+        
+        var transform: CGAffineTransform = CGAffineTransformIdentity
+        
+        switch self.imageOrientation {
+        case UIImageOrientation.Down, UIImageOrientation.DownMirrored:
+            transform = CGAffineTransformTranslate(transform, self.size.width, self.size.height)
+            transform = CGAffineTransformRotate(transform, CGFloat(M_PI))
+            break
+        case UIImageOrientation.Left, UIImageOrientation.LeftMirrored:
+            transform = CGAffineTransformTranslate(transform, self.size.width, 0)
+            transform = CGAffineTransformRotate(transform, CGFloat(M_PI_2))
+            break
+        case UIImageOrientation.Right, UIImageOrientation.RightMirrored:
+            transform = CGAffineTransformTranslate(transform, 0, self.size.height)
+            transform = CGAffineTransformRotate(transform, CGFloat(-M_PI_2))
+            break
+        case UIImageOrientation.Up, UIImageOrientation.UpMirrored:
+            break
+        }
+        
+        switch self.imageOrientation {
+        case UIImageOrientation.UpMirrored, UIImageOrientation.DownMirrored:
+            CGAffineTransformTranslate(transform, self.size.width, 0)
+            CGAffineTransformScale(transform, -1, 1)
+            break
+        case UIImageOrientation.LeftMirrored, UIImageOrientation.RightMirrored:
+            CGAffineTransformTranslate(transform, self.size.height, 0)
+            CGAffineTransformScale(transform, -1, 1)
+        case UIImageOrientation.Up, UIImageOrientation.Down, UIImageOrientation.Left, UIImageOrientation.Right:
+            break
+        }
+        
+        let ctx:CGContextRef = CGBitmapContextCreate(nil, Int(self.size.width), Int(self.size.height), CGImageGetBitsPerComponent(self.CGImage), 0, CGImageGetColorSpace(self.CGImage), CGImageAlphaInfo.PremultipliedLast.rawValue)!
+        
+        CGContextConcatCTM(ctx, transform)
+        
+        switch self.imageOrientation {
+        case UIImageOrientation.Left, UIImageOrientation.LeftMirrored, UIImageOrientation.Right, UIImageOrientation.RightMirrored:
+            CGContextDrawImage(ctx, CGRectMake(0, 0, self.size.height, self.size.width), self.CGImage)
+            break
+        default:
+            CGContextDrawImage(ctx, CGRectMake(0, 0, self.size.width, self.size.height), self.CGImage)
+            break
+        }
+        
+        let cgimg:CGImageRef = CGBitmapContextCreateImage(ctx)!
+        let img:UIImage = UIImage(CGImage: cgimg)
+        
+        return img
+    }
+}
